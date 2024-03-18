44a1646dd7c33c8b17769ed37aeddc3255ea8336
ianmaciel0612 <161033380+ianmaciel0612@users.noreply.github.com>
2024-03-18 08:45:31 -0300

    Add files via upload

---
+++ dev c.c
@@ -0,0 +1,25 @@
+#include<studio.h>
+
+int main(){
+	float primeiroNumero, segundoNumero;
+	float soma, subtracao, multiplicacao, divisao,;
+
+	printf("escreva o numero 1: ");
+	scanf("%f",&primeiroNumero);
+	
+	printf("digite o numero 2: ");
+	scanf("%f" ,&segundoNumero);
+	
+	
+	
+	soma = primeiroNumero + segundoNumero;
+	subtracao = primeiroNumero - segundoNumero;
+	multiplicacao = primeiroNumero * segundoNumero
+	divisao = primeiroNumero / segundoNumero
+	
+	//system("cls"); // limpa o terminal no windows 
+	//system ("clear"); // limpa o terminal no linux
+	
+	pritf("\n===exibindo resultados===");
+	prinft("\n soma")
+	}
