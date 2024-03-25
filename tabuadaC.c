64045a6ce6489b2664784cc7dc3debbef484c8f6
ianmaciel0612 <161033380+ianmaciel0612@users.noreply.github.com>
2024-03-25 08:33:38 -0300

    Add files via upload

---
+++ tabuadaC.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <locale.h>
+
+	int main(){
+		setlocale(LC_ALL, "");
+		int i;
+			
+			printf("===\n tabuada do numero 2===");
+			for(i = 1; i<= 10; i++) {
+				printf("%d x %d = %d \n", 3, i, i*3 );
+			}
+			
+				printf("===\n tabuada do numero 2 ");
+			for(i=1;i<=10;i++){
+		printf("%d x %d = %d \n", 2, i, i*2);
+	}
+	
+					printf("===\n tabuada do numero 2 ");
+			for(i=1;i<=10;i++){
+		printf("%d x %d = %d \n", 5, i, i*5);
+	}
+		
+		return 0;
+	}
